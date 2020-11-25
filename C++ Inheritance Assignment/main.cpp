cb0ede2a29768e23869aebe002a4e194b87b61d3
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-11-24 19:11:23 -0500

    Initial Commit

---
+++ C++ Inheritance Assignment/main.cpp
@@ -0,0 +1,13 @@
+// Andrew Biddle
+// CIS 1202.501
+// 11.24.20
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // insert code here...
+    cout << endl << endl;
+    return 0;
+}
